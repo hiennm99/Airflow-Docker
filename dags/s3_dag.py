d7384be4304e364c131548e383e5e581736112ad
hiennm99 <hiennm11599@gmail.com>
2022-11-29 15:56:04 +0700

    update S3

--- dags/s3_dag.py
+++ dags/s3_dag.py
@@ -1,27 +1,30 @@
+# dags/my_dag.py
+
 from airflow import DAG
-from airflow.decorators import task,dag
+from airflow.decorators import task, dag
 
 from datetime import datetime
 import pandas as pd
 
-
 @task
-def extract():  
+def extract():
+
     people = {
         'Firstnames': ['James','Corolla','Mark','Eddy'],
-        'Lastnames':['Wick','Leto','Smith','Etwan']
+        'Lastnames': ['Wick','Leto','Smith','Etwan']
     }
-    
-    df= pd.DataFrame(people,columns=['Firstnames','Lastnames'])
+
+    df = pd.DataFrame(people, columns = ['Firstnames', 'Lastnames'])
     return df
 
 @task
 def process(value):
     print(f"Processing: {value}")
-    
-@dag(schedule_interval='@daily',start_date=datetime(2022,1,1),catchup=False)
+
+@dag(schedule_interval='@daily',start_date=datetime(2021, 1, 1), catchup=False)
 def s3_dag():
-    value=extract()
+
+    value = extract()
     process(value)
-    
-dag=s3_dag()
+
+dag = s3_dag()
